22d66aacbe9f901ad6bdf221aae86a6ad7cd297b
Manish Chellappan <tabnexus12@gmail.com>
2021-10-10 19:23:19 +0530

    updated

--- testDockLib.py
+++ testDockLib.py
@@ -7,7 +7,10 @@ client=dock_login()
 images=dock_get_images(client)
 
 
-#cont=create_containers(client,'nginx:1.15')
+#cont=create_containers(client,'nginx:1.18')
+#cont=create_containers(client,'nginx:1.20')
+#cont=create_containers(client,'nginx:1.20')
+#cont=create_containers(client,'nginx:1.20')
 #print("Name:"+cont.attrs['Name'] + " "+"Id:"+cont.attrs['Id']+" " + "Status:" + " " + cont.attrs['State']['Status'])
 
 #stop_container(client,cont)
@@ -15,11 +18,11 @@ images=dock_get_images(client)
 
 get_all_images(client)
 
-current_ver_containers=get_all_containers(client,images[2])
+current_ver_containers=get_all_containers(client,images[0])
 # for cont in current_ver_containers:
 #     stop_container(client,cont)
 
 # client.containers.prune()
-#print(current_ver_containers)
+print(current_ver_containers)
 
-print(deploy_rollout(client,images[2],images[1]))
+print(deploy_rollout(client,'nginx:1.15','nginx:1.21'))
